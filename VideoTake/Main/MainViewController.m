ee958eee803b1bad59bc0907e9fcb3ec284817f9
wanghuizhou <wanghuizhou@guazi.com>
2018-02-06 11:03:56 +0800

    添加视频拍摄入口

---
+++ VideoTake/Main/MainViewController.m
@@ -0,0 +1,25 @@
+//
+//  MainViewController.m
+//  VideoTake
+//
+//  Created by zzg on 2018/2/6.
+//  Copyright © 2018年 zzg. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"视频拍摄";
+    
+    
+}
+
+
+@end
